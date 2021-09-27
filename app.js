439dc17c64733468f10557c875f34fbc80133d47
aroyehunahmad <ahmadadewale800@gmail.com>
2021-09-27 17:19:29 +0100

    Test an open mongo link

--- app.js
+++ app.js
@@ -10,10 +10,9 @@ const config = require('./utils/config')
 const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
 
-mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+mongoose.connect('mongodb+srv://ahmad:22juneC@cluster0.cxs4b.mongodb.net/blog-list?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
  .then(() => logger.info('connected to MongoDB'))
  .catch(error => logger.error('error connection to MongoDB:', error.message))
- console.log('Mon', config.MONGODB_URI)
 app.use(cors())
 app.use(express.json())
 
